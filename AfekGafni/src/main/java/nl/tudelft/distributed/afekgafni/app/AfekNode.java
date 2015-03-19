0f690504a962d3ecf7a1eb3fe303f534c0d1bab1
jabbink <git@abb.ink>
2015-03-19 12:50:41 +0100

    Format Afek Gafni code

--- AfekGafni/src/main/java/nl/tudelft/distributed/afekgafni/app/AfekNode.java
+++ AfekGafni/src/main/java/nl/tudelft/distributed/afekgafni/app/AfekNode.java
@@ -1,54 +1,46 @@
 package nl.tudelft.distributed.afekgafni.app;
 
+import nl.tudelft.distributed.afekgafni.process.Candidate;
+import nl.tudelft.distributed.afekgafni.process.Ordinary;
+
 import java.net.MalformedURLException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 
-import nl.tudelft.distributed.afekgafni.process.Candidate;
-import nl.tudelft.distributed.afekgafni.process.Ordinary;
-
 public class AfekNode {
 	public static final String USAGE = "usage: id isCandidate otherNodes*";
-	public static final int RMI_PORT = 1337;
-	
-	
+
 	public static void main(String[] args) {
-		if(args.length >= 2){
-			try{
+		if (args.length >= 2) {
+			try {
 				int nodeId = Integer.parseInt(args[0]);
 				boolean isCandidate = args[1].toLowerCase().equals("candidate");
 				String[] otherNodes = new String[args.length - 2];
 				System.arraycopy(args, 2, otherNodes, 0, otherNodes.length);
 				Thread.sleep(5000);
 				start(nodeId, isCandidate, otherNodes);
-			} catch(NumberFormatException e){
+			} catch (NumberFormatException e) {
 				System.out.println(USAGE);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-		} else{
+		} else {
 			System.out.println(USAGE);
 		}
 	}
-	
-	public static void start(int nodeId, boolean isCandidate, String[] otherNodes){
-		try{
+
+	public static void start(int nodeId, boolean isCandidate, String[] otherNodes) {
+		try {
 			Ordinary ordinary = new Ordinary(nodeId);
 			Naming.bind(ordinary.getRemote(), ordinary);
-			
-			if(isCandidate){
+
+			if (isCandidate) {
 				Candidate candidate = new Candidate(nodeId, otherNodes);
 				candidate.startElection();
 			}
-		} catch(RemoteException | MalformedURLException | AlreadyBoundException e){
+		} catch (RemoteException | MalformedURLException | AlreadyBoundException e) {
 			e.printStackTrace();
-			return;
 		}
-		
-		//Naming.bind(name, obj);
-		
-		
-		
 	}
 }
