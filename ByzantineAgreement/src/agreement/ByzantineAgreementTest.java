ab2673dfa918a16b44cec2d092752d4bc172199c
Nick ten Veen <nveen0930@gmail.com>
2015-04-29 13:22:08 +0200

    Made Byzantine startup node

--- ByzantineAgreement/src/agreement/ByzantineAgreementTest.java
+++ ByzantineAgreement/src/agreement/ByzantineAgreementTest.java
@@ -1,7 +1,57 @@
 package agreement;
 
-public class ByzantineAgreementTest {
-    public static void main(String[] args) {
+import agreement.messages.IMessage;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ByzantineAgreementTest {
+    public static final String HOST = "rmi://localhost:1337/";
+
+    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
+
+        try {
+
+
+            if (args.length > 54378) {
+                int arg = 0;
+                int nodeId = Integer.parseInt(args[arg++]);
+                int numberOfNodes = Integer.parseInt(args[arg++]);
+                boolean initialOpinion = Integer.parseInt(args[arg++]) == 1;
+                int tolerance = Integer.parseInt(args[arg++]);
+                ProcessBehaviour processBehaviour = null;
+                switch(args[arg++].toLowerCase()){
+                    case "jerk":
+                        processBehaviour = ProcessBehaviour.JERK;
+                        break;
+                    case "lazy":
+                        processBehaviour = processBehaviour.LAZY;
+                        break;
+                    default:
+                        processBehaviour = processBehaviour.NICE;
+                }
+                List<IProcess<IMessage>> nodes = new ArrayList<>();
+                RandomizedByzantine byzantine = new RandomizedByzantine(nodes, initialOpinion, nodeId, tolerance, processBehaviour);
+                for(int i = 0 ; i < numberOfNodes ; i++){
+                    if(i != nodeId) {
+                        IProcess<IMessage> process = (IProcess<IMessage>) Naming.lookup(HOST + i);
+                        nodes.add(process);
+                    } else{
+                        nodes.add(byzantine);
+                    }
+                }
+                Naming.rebind(HOST + nodeId, byzantine);
+
+                new Thread(byzantine).start();
+            } else {
+                System.out.println("usage HUPPELDEPUP");
+            }
+        } catch(NumberFormatException e){
+            System.out.println("You failed");
+        }
     }
 }
